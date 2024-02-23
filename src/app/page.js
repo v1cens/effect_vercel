7ed3aa28c59f68ea468753dcb9480f287eaefd3b
v1cens <121035299+v1cens@users.noreply.github.com>
2024-02-23 11:37:17 +0100

    Votre message de commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,56 @@
-'use client';
-import styles from './page.module.css'
-import { useState } from 'react';
-import Project from '../components/project';
-import Modal from '../components/modal';
+"use client";
+import styles from "./page.module.css";
+import { useState } from "react";
+import Project from "../components/project";
+import Modal from "../components/modal";
 
 const projects = [
   {
-    title: "C2 Montreal",
+    title: "Data.ai",
     src: "c2montreal.png",
-    color: "#000000"
+    color: "#400000",
+    link: "https://data.ai",
   },
   {
-    title: "Office Studio",
+    title: "Pixloc",
     src: "officestudio.png",
-    color: "#8C8C8C"
+    color: "#8C8C8C",
+    link: "https://pixloc.fr",
   },
   {
-    title: "Locomotive",
+    title: "Okho",
     src: "locomotive.png",
-    color: "#EFE8D3"
+    color: "#EFE8D3",
+    link: "https://okho-architectures.fr/",
   },
   {
-    title: "Silencio",
+    title: "Grenadine",
     src: "silencio.png",
-    color: "#706D63"
-  }
-]
+    color: "#706D63",
+    link: "https://grenadinehair.com/",
+  },
+];
 
 export default function Home() {
-
-  const [modal, setModal] = useState({active: false, index: 0})
+  const [modal, setModal] = useState({ active: false, index: 0 });
+  const activeProject = projects[modal.index];
 
   return (
-  <main className={styles.main}>
-    <div className={styles.body}>
-      {
-        projects.map( (project, index) => {
-          return <Project index={index} title={project.title} setModal={setModal} key={index}/>
-        })
-      }
-    </div>
-    <Modal modal={modal} projects={projects}/>
-  </main>
-  )
+    <main className={styles.main}>
+      <img className={styles.logo} src="/logo.svg" alt="Logo" />
+      <div className={styles.body}>
+        {projects.map((project, index) => {
+          return (
+            <Project
+              index={index}
+              title={project.title}
+              setModal={setModal}
+              key={index}
+            />
+          );
+        })}
+      </div>
+      <Modal modal={modal} projects={projects} activeProject={activeProject} />
+    </main>
+  );
 }
